d340a828085a391f323ba30a2d40fb7b8d7c4480
Маматкулов <farmamatkulov@mail.ru>
2022-09-05 12:17:05 +0700

    Add project files.

---
+++ WpfApp6/AppViewModel.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+
+namespace WpfApp6
+{
+    public class AppViewModel
+    {
+        private MathModel _model;
+        private string _selectedFunction;
+
+        public List<MathModel> ModelsList { get; set; }
+        public BindingList<string> Functions { get; set; }
+        public List<int> Сoefficients { get; set; }
+
+
+        private RelayCommand command;
+        public RelayCommand Command
+        {
+            get
+            {
+                return command ?? (command = new RelayCommand(obj =>
+                System.Diagnostics.Trace.WriteLine(
+                   _model.A + _model.B + _model.C)));
+            }
+        }
+
+        public MathModel Model
+        {
+            get { return _model; }
+            set { _model = value; }
+        }
+
+        public string SelectedFunction
+        {
+            get { return _selectedFunction; }
+            set
+            {
+                _selectedFunction = value;
+                System.Diagnostics.Trace.WriteLine(_selectedFunction);
+            }
+        }
+
+        public AppViewModel()
+        {
+            ModelsList = new List<MathModel>
+            {
+                new MathModel()
+            };
+
+            _model = new MathModel();
+            Functions = new BindingList<string> { 
+                new string("Линейная"),
+                new string("Квадратичная"), 
+                new string("Кубическая"),
+                new string("4-ой степени"),
+                new string("5-ой степени")
+            };
+
+            Сoefficients = new List<int>
+            {
+                1,
+                2,
+                3,
+                4,
+                5
+            };
+        }
+    }
+}
---
+++ WpfApp6/MathModel.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace WpfApp6
+{
+    public class MathModel: INotifyPropertyChanged
+    {
+        private int _a;
+        private int _b;
+        private int _c;
+        private int _x;
+        private int _y;
+        private int _fxy;
+
+        public int A
+        {
+            get { return _a; }
+            set
+            {
+                _a = value;
+                OnPropertyChanged("A");
+                System.Diagnostics.Trace.WriteLine(_a);
+            }
+        }
+
+        public int B
+        {
+            get { return _b; }
+            set
+            {
+                _b = value;
+                OnPropertyChanged("B");
+            }
+        }
+
+        public int C
+        {
+            get { return _c; }
+            set
+            {
+                _c = value;
+                OnPropertyChanged("C");
+                System.Diagnostics.Trace.WriteLine(_c);
+            }
+        }
+
+        public int X
+        {
+            get { return _x; }
+            set
+            {
+                _x = value;
+                OnPropertyChanged("X");
+            }
+        }
+
+        public int Y
+        {
+            get { return _y; }
+            set
+            {
+                _y = value;
+                OnPropertyChanged("Y");
+            }
+        }
+        
+        public int Fxy
+        {
+            get { return _fxy; }
+            set
+            {
+                _fxy = value;
+                OnPropertyChanged("Fxy");
+            }
+        }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        }
+    }
+}
